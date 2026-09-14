f3f49d29387b10fecea0a29c5c7f61513877dd3a
alejandro2397 <72469464+alejandro2397@users.noreply.github.com>
2026-09-14 15:36:38 -0600

    Add Product Room entity

---
+++ app/src/main/java/com/alejandro/minegociosv/data/Product.kt
@@ -0,0 +1,13 @@
+package com.alejandro.minegociosv.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class Product(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val name: String,
+    val cost: Double,
+    val price: Double,
+    val stock: Double
+)
